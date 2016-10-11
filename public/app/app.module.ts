22e5ff6a6a33a44d1bc1f2f89b5016349478ba29
Anthony Sitaram <anthony.sitaram@hva.nl>
2016-10-11 14:57:49 +0200

    sadfasdf

--- public/app/app.module.ts
+++ public/app/app.module.ts
@@ -4,10 +4,11 @@ import {AppComponent}   from './components/app.component';
 import {HeaderComponent} from "./components/header.component";
 import {CarouselComponent} from "./components/carousel.component";
 import {UploadSlideShowComponent} from "./components/uploadSlideShow.component";
+import {FileDropDirective, FileSelectDirective} from "ng2-file-upload/ng2-file-upload";
 
 @NgModule({
     imports: [BrowserModule],
-    declarations: [AppComponent, HeaderComponent, CarouselComponent, UploadSlideShowComponent],
+    declarations: [AppComponent, HeaderComponent, CarouselComponent, UploadSlideShowComponent, FileDropDirective, FileSelectDirective],
     bootstrap: [AppComponent]
 })
 export class AppModule {
