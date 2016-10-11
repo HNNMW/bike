15ed8f103404137f2e1ce77984312c4ec50c8a11
Anthony Sitaram <anthony.sitaram@hva.nl>
2016-10-11 13:15:33 +0200

    uploadSlideShow

--- public/app/app.module.ts
+++ public/app/app.module.ts
@@ -3,9 +3,10 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent}   from './components/app.component';
 import {HeaderComponent} from "./components/header.component";
 import {CarouselComponent} from "./components/carousel.component";
+import {UploadSlideShowComponent} from "./components/uploadSlideShow.component";
 @NgModule({
     imports: [BrowserModule],
-    declarations: [AppComponent, HeaderComponent, CarouselComponent],
+    declarations: [AppComponent, HeaderComponent, CarouselComponent, UploadSlideShowComponent],
     bootstrap: [AppComponent]
 })
 export class AppModule {
