269d01130fea917b6344ee0228969137dc013f1b
cigirci <fatih@cigirci.com>
2016-10-28 21:21:06 +0200

    andere css stijl voor descriptie tonen

--- public/app/components/gallery.component.ts
+++ public/app/components/gallery.component.ts
@@ -14,9 +14,13 @@ export class GalleryComponent {
     public images = images;
     enlargedImageUrl : string;
     displayLargeImage : boolean;
+    desc : string;
+    title : string;
 
-    public enlargeImage = function (id){
+    public enlargeImage = function (id, desc, title){
         this.enlargedImageUrl = id;
+        this.desc = desc;
+        this.title = title;
         this.displayLargeImage = true;
     }
 
@@ -28,7 +32,7 @@ export class GalleryComponent {
 }
 
 var images: galleryImage[] = [
-    { "url": "/images/bike2.jpg", title: "asd", description: "asd" },
+    { "url": "/images/bike2.jpg", title: "Green dare-devil fighter!", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc rutrum sit amet odio ullamcorper scelerisque. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut quis nisl ligula. Etiam aliquam luctus nulla in euismod. Donec libero ipsum, posuere nec malesuada vitae, tempus euismod justo." },
     { "url": "/images/bike2.jpg", title: "asd", description: "asd" },
     { "url": "/images/bike2.jpg", title: "asd", description: "asd" },
     { "url": "/images/bike2.jpg", title: "asd", description: "asd" },
