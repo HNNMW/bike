ad9faecc6774ca1676a756dab7a529a7f7cc90f1
DESKTOP-MMQLEEF\Bob <bobvantpadje>
2016-10-28 23:38:10 +0200

    Changed the layout a bit

--- public/app/components/gallery.component.ts
+++ public/app/components/gallery.component.ts
@@ -1,38 +1,37 @@
 /**
  * Created by Bob on 8-10-2016.
  */
-import { Component } from '@angular/core';
+import { Component,  trigger, transition, style, animate, state } from '@angular/core';
 import { galleryImage } from '../interfaces/galleryImage.interface';
 
 @Component({
     selector: 'gallery',
     styleUrls: ['app/style/gallery.css'],
     templateUrl: '/app/views/gallery.html',
+
 })
 
 export class GalleryComponent {
     public images = images;
+    selectedImage: galleryImage;
     enlargedImageUrl : string;
     displayLargeImage : boolean;
     desc : string;
     title : string;
 
-    public enlargeImage = function (id, desc, title){
-        this.enlargedImageUrl = id;
-        this.desc = desc;
-        this.title = title;
+    public enlargeImage = function (image : galleryImage ){
+        this.selectedImage = image;
         this.displayLargeImage = true;
     }
 
     public closeImage = function (){
         this.displayLargeImage = false;
-        console.log("test");
     }
 
 }
 
 var images: galleryImage[] = [
-    { "url": "/images/bike2.jpg", title: "Green dare-devil fighter!", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc rutrum sit amet odio ullamcorper scelerisque. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut quis nisl ligula. Etiam aliquam luctus nulla in euismod. Donec libero ipsum, posuere nec malesuada vitae, tempus euismod justo." },
+    { "url": "/images/bike2.jpg", title: "Dikke leipe bika", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc rutrum sit amet odio ullamcorper scelerisque. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut quis nisl ligula. Etiam aliquam luctus nulla in euismod. Donec libero ipsum, posuere nec malesuada vitae, tempus euismod justo." },
     { "url": "/images/bike2.jpg", title: "asd", description: "asd" },
     { "url": "/images/bike2.jpg", title: "asd", description: "asd" },
     { "url": "/images/bike2.jpg", title: "asd", description: "asd" },
