669c2112fc6f852ac07f3b60bb3f3c7e1f6e80f5
DESKTOP-MMQLEEF\Bob <bobvantpadje>
2016-10-31 22:49:35 +0100

    started making the website responsive

--- public/app/components/header.component.ts
+++ public/app/components/header.component.ts
@@ -5,5 +5,18 @@ import { Component } from '@angular/core';
     templateUrl: '/app/views/header.html',
 })
 export class HeaderComponent {
+   hideHeader : boolean;
+    previousScroll : number = 0;
+
+    onScroll() {
+        if (window.scrollY > 50)
+            this.hideHeader = true;
+
+        // Show header when scrolling up
+        if(window.scrollY < this.previousScroll)
+            this.hideHeader = false;
+
+        this.previousScroll = window.scrollY;
+    }
 
 }
