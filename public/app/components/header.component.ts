22ec0a162c1e5f05ae4f5216c503ba7245423e82
DESKTOP-MMQLEEF\Bob <bobvantpadje>
2016-10-31 22:51:20 +0100

    started making the website responsive

--- public/app/components/header.component.ts
+++ public/app/components/header.component.ts
@@ -1,19 +1,24 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 @Component({
     selector: 'header',
     styleUrls: ['app/style/header.css'],
     templateUrl: '/app/views/header.html',
 })
 export class HeaderComponent {
-   hideHeader : boolean;
-    previousScroll : number = 0;
+    hideHeader: boolean;
+    openDropdown = false;
+    previousScroll: number = 0;
+
+    toggleDropdown = function () {
+        this.openDropdown = !this.openDropdown
+    }
 
     onScroll() {
         if (window.scrollY > 50)
             this.hideHeader = true;
 
         // Show header when scrolling up
-        if(window.scrollY < this.previousScroll)
+        if (window.scrollY < this.previousScroll)
             this.hideHeader = false;
 
         this.previousScroll = window.scrollY;
