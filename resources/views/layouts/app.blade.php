64632d0d6f7d53125d939f7ab646a1503931499f
bobvantpadje <bobvantadje@gmail.com>
2017-01-11 17:15:02 +0100

    Edited some texts and hrefs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,7 +41,7 @@
 
                 <!-- Branding Image -->
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    STUCKINSTEEL
                 </a>
             </div>
 
